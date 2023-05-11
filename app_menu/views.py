41e18e92c4c0576bb98c6aa8b09887d53fd97d12
Timofey Borovets <111517050+sonic-tim@users.noreply.github.com>
2023-05-11 09:54:34 +0300

    Update views.py
    Добавлено значение по умолчанию для pk равное None

--- app_menu/views.py
+++ app_menu/views.py
@@ -5,7 +5,7 @@ from django.views.generic import View
 class IndexView(View):
 
     @classmethod
-    def get(cls, request, pk):
+    def get(cls, request, pk=None):
         return render(
             request,
             template_name='index.html'
